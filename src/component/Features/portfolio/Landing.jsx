159e9d91605a991740d988e1da56a6e600790525
Ishwar <ishwar.official0001@gmail.com>
2025-11-25 17:25:18 +0530

    Update PricePage, HomePage, AboutPage, ContactPage

--- src/component/Features/portfolio/Landing.jsx
+++ src/component/Features/portfolio/Landing.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
-import PortfolioText from './PortfolioText';
+// import PortfolioText from './PortfolioText';
+import Hero from '../PortfolioPage/Hero'
 import Portfolio from './Portfolio';
 
 
 export default function Landing() {
   return (
     <div>
-      <PortfolioText/>
+      
+      {/* <PortfolioText/> */}
+      <Hero/>
       <Portfolio/>
     </div>
   )
